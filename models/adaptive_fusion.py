6fd1c0c50b819813534ed9f850db36e8623e9958
xugenrui <2350080984@qq.com>
2025-05-04 13:39:55 +0800

    Update adaptive_fusion.py

--- models/adaptive_fusion.py
+++ models/adaptive_fusion.py
@@ -18,29 +18,37 @@ class Adaptive_fusion(nn.Module):
         self.dec_embedding = DataEmbedding(configs.ts_dec_in, configs.ts_d_model, configs.ts_embed, configs.freq,
                                            configs.dropout)
         
-        self.gate = nn.Linear(configs.ts_d_model * configs.pred_len, configs.ts_d_model * configs.pred_len, bias=True)
         self.sigmoid = nn.Sigmoid()
-        if configs.ts_distil:
-            self.reg = nn.Linear(configs.ts_d_model * (configs.seq_len // 2 + 1), configs.ts_d_model * configs.pred_len, bias=True)
-            self.reg_cs = nn.Linear(configs.ts_d_model * (configs.seq_len // 2 + 1), configs.ts_d_model * configs.pred_len, bias=True)
+        if args.ts_distil:
+            self.reg_cs_step1 = nn.Linear(args.ts_d_model, args.ts_d_model, bias=True)
+            self.reg_cs_step2 = nn.Linear((args.seq_len // 2 + 1), args.pred_len, bias=True)
+            
+            self.reg_step1 = nn.Linear(args.ts_d_model, args.ts_d_model, bias=True)
+            self.reg_step2 = nn.Linear((args.seq_len // 2 + 1), args.pred_len, bias=True)
+
         else:
-            self.reg = nn.Linear(configs.ts_d_model * configs.seq_len, configs.ts_d_model * configs.pred_len, bias=True)
-            self.reg_cs = nn.Linear(configs.ts_d_model * configs.seq_len, configs.ts_d_model * configs.pred_len, bias=True)
+            self.reg_step1 = nn.Linear(args.ts_d_model, args.ts_d_model, bias=True)
+            self.reg_step2 = nn.Linear(args.seq_len, args.pred_len, bias=True)
+
+            self.reg_cs_step1 = nn.Linear(args.ts_d_model, args.ts_d_model, bias=True)
+            self.reg_cs_step2 = nn.Linear(args.seq_len, args.pred_len, bias=True)
         
 
     def forward(self, ts_enc_x, img_enc_x, dec_inp, dec_inp_mark):
         _, L, _ = ts_enc_x.shape
 
-        ts_enc_x = rearrange(ts_enc_x, 'b t d -> b (t d)')
-
         unit = ts_enc_x.detach()
-        unit = self.reg_cs(unit)
+        unit = self.reg_cs_step1(unit)
+        unit = rearrange(unit, 'b t d -> b d t')
+        unit = self.reg_cs_step2(unit)
+        unit = rearrange(unit, 'b d t -> b t d')
         
-        ts_enc_x = self.reg(ts_enc_x)
+        ts_enc_x = self.reg_step1(ts_enc_x)
+        ts_enc_x = rearrange(ts_enc_x, 'b t d -> b d t')
+        ts_enc_x = self.reg_step2(ts_enc_x)
+        ts_enc_x = rearrange(ts_enc_x, 'b d t -> b t d')
 
-        ts_enc_x_gate = self.sigmoid(self.gate(unit))
-        ts_enc_x_gate = rearrange(ts_enc_x_gate, 'b (t d) -> b t d', t=self.pred_len)
-        ts_enc_x = rearrange(ts_enc_x, 'b (t d) -> b t d', t=self.pred_len)
+        ts_enc_x_gate = self.sigmoid(unit)
 
         dec_info = self.dec_embedding(dec_inp, dec_inp_mark)
         dec_info = dec_info[:, -self.pred_len:, :]
